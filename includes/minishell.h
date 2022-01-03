fe11d825abb88da1ea0f49f386b7aa5261cab5d9
adonis <adonissfy42@gmail.com>
2022-01-03 12:37:08 +0100

    attempt to free exec_cmd

--- includes/minishell.h
+++ includes/minishell.h
@@ -17,6 +17,7 @@
 # include "../libft/libft.h"
 # include "../ft_printf/ft_printf.h"
 # include "../get_next_line/get_next_line.h"
+# include "./pipex.h"
 
 typedef struct s_process
 {
--- srcs/execute_cmd.c
+++ srcs/execute_cmd.c
@@ -1,16 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   execute_cmd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asaffroy <asaffroy@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 11:15:22 by asaffroy          #+#    #+#             */
-/*   Updated: 2022/01/03 11:36:20 by asaffroy         ###   ########lyon.fr   */
+/*   Updated: 2022/01/03 12:34:32 by asaffroy         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/pipex.h"
+#include "../includes/minishell.h"
+
+/*
+*int	find_path(char **env)
+*env	env of the shell
+*/
 
 int	find_path(char **env)
 {
@@ -25,6 +30,13 @@ int	find_path(char **env)
 	}
 	return (i);
 }
+
+/*
+*char	*ft_check_arg(char *cmd, char **env)
+*cmd	command to search in /bin
+*env	env of the shell
+*error	not norminette
+*/
 
 char	*ft_check_arg(char *cmd, char **env)
 {
@@ -47,12 +59,32 @@ char	*ft_check_arg(char *cmd, char **env)
 	{
 		try = ft_strjoin(tab[i], cmd);
 		if (!access(try, X_OK))
+		{
+			i = -1;
+			while (tab[++i])
+				free(tab[i]);
+			free(tab);
+			free(cmd);
 			return (try);
+		}
 		i++;
 	}
+	i = -1;
+	while (tab[++i])
+		free(tab[i]);
+	free(tab);
+	free(cmd);
+	free(try);
 	ft_perror("command not found");
 	return (0);
 }
+
+/*
+*void	father_proc(t_data *data, char **argv, char **env)
+*data	my struct with all data needed
+*env	env of the shell
+*argv	tab of args received at launch of 
+*/
 
 void	father_proc(t_data *data, char **argv, char **env)
 {
@@ -91,10 +123,11 @@ void	child_proc(t_data *data, char **argv, char **env)
 		ft_perror("failed to exec in child_proc");
 }
 
-int	ft_execute_cmd(int argc, char **argv, char **env, t_talistechainee *lambda)
+int	ft_execute_cmd(int argc, char **argv, char **env, t_process *proc)
 {
 	int			pid1;
 	t_data		data;
+	int			i;
 
 	if (argc != 5)
 		ft_perror
@@ -111,5 +144,19 @@ int	ft_execute_cmd(int argc, char **argv, char **env, t_talistechainee *lambda)
 		else
 			father_proc(&data, argv, env);
 	}
+	i = -1;
+	while (data.args_of_1[++i])
+	{
+		free(data.args_of_1);
+		i++;
+	}
+	i = -1;
+	while (data.args_of_2[++i])
+	{
+		free(data.args_of_2);
+		i++;
+	}
+	free(data.path_of_1);
+	free(data.path_of_2);
 	return (0);
 }
