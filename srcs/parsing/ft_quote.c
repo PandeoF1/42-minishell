acb5d701fac4b8f8b8b9a1753684206a52c1e94d
tnard <theo.nard18@gmail.com>
2022-01-20 17:15:54 +0100

    norminette

--- srcs/parsing/ft_quote.c
+++ srcs/parsing/ft_quote.c
@@ -6,7 +6,7 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 11:28:24 by tnard             #+#    #+#             */
-/*   Updated: 2022/01/20 12:44:59 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2022/01/20 17:14:34 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,17 @@ int	ft_len_next(char *str)
 }
 
 /*
+* void	ft_made_it(int *i, char *c, char x)
+* desc : config deux var car j'ai pas la place
+*/
+
+static void	ft_made_it(int *i, char *c, char x)
+{
+	*i = 0;
+	*c = x;
+}
+
+/*
  * int ft_quote(t_process **process, int x, int y, char **split)
  * desc : parse the curent line after the <> and return the new process
  * todo : free(process) if err ?
@@ -70,8 +81,7 @@ void	ft_quote(t_process **process, int x, int *y, char **split)
 	int		i;
 	t_inout	*tmp;
 
-	i = 0;
-	c = split[x][(*y)];
+	ft_made_it(&i, &c, split[x][(*y)]);
 	tmp = ft_create_inout();
 	if (split[x][(*y) + 1] && (split[x][(*y) + 1] == c))
 	{
@@ -90,6 +100,6 @@ void	ft_quote(t_process **process, int x, int *y, char **split)
 	}
 	(*y) += ft_w_is_space(split[x] + (*y) + 1) + 1;
 	tmp->file = ft_strndup(split[x] + (*y), ft_len_quote(split[x] + (*y)));
-	(*y) += ft_len_next(split[x] + (*y));
+	(*y) += ft_len_quote(split[x] + (*y));
 	ft_push_inout(&(*process)->inout, tmp);
 }
