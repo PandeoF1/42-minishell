fa438c09696013bba02cf81819d4ef9ee3d0b50a
adonis <adonissfy42@gmail.com>
2022-03-30 18:58:10 +0200

    fix

--- srcs/built-in/ft_cd.c
+++ srcs/built-in/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: asaffroy <asaffroy@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 14:55:26 by asaffroy          #+#    #+#             */
-/*   Updated: 2022/02/02 09:10:19 by asaffroy         ###   ########lyon.fr   */
+/*   Updated: 2022/03/30 18:55:04 by asaffroy         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	ft_cd(t_data *data, int fd, char **path)
 	else if (ft_strstrlen(path) == 2)
 	{
 		if (chdir(path[1]))
-			ft_putstr_fd("minishell: cd: No such file or directory\n", 2);
+			ft_putstr_fd("minishell: cd: Not a directory\n", 2);
 	}
 	else
 		ft_putstr_fd("wrong number of arguments\n", 2);
